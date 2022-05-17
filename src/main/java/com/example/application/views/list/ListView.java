c088dd17565df26a1f73b102de40e41769bf41a1
Pownyfals <pownyfals@gmail.com>
2022-05-17 16:04:57 +0700

    menambahkan add contact form

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,11 +1,15 @@
 package com.example.application.views.list;
 
+import java.util.Collections;
+
 import com.example.application.data.entity.Contact;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -14,12 +18,47 @@ import com.vaadin.flow.router.Route;
 public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class); 
     TextField filterText = new TextField();
+    ContactForm form;
 
     public ListView() {
         addClassName("list-view");
         setSizeFull();
 
         configureGrid();
+        configureForm();
+
+        add(
+            getToolbar(),
+            getContent()
+        );
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid,form);
+        content.setFlexGrow(2,grid);
+        content.setFlexGrow(1,form);
+        content.addClassName("content");
+        content.setSizeFull();
+
+        return content;
+    }
+
+    private void configureForm() {
+        form = new ContactForm(Collections.emptyList(), Collections.emptyList());
+        form.setWidth("25em");
+
+    }
+
+    private Component getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button addContactButton = new Button("Add Contact");
+        
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
     }
 
     private void configureGrid() {
